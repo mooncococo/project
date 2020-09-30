68ecf4553716e043b4aec8a023bd4deef82ccc5c
lsa <lsa@DESKTOP-SHD7BQ5>
2020-09-30 14:00:10 +0900

    lsa2

--- src/main/java/com/sping/ex/test5.java
+++ src/main/java/com/sping/ex/test5.java
@@ -3,8 +3,7 @@ package com.sping.ex;
 public class test5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Å×½ºÆ®ÁßÀÔ´Ï´Ù.");
+		System.out.println("ì•ˆë…•í•˜ì„¸ìš” ");
 	}
 
 }
